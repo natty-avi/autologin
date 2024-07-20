0a57842d45b70460cd3b917efae8afd955632bdf
Natty <avi100147@gmail.com>
2024-07-21 00:11:49 +0530

    updated controller to handle the new fields:

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -5,7 +5,10 @@ const {
     updatePartner,
     deletePartner
 } = require('../models/Partner');
+
+const pool = require('../config/db');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const handleCreatePartner = async (req, res) => {
     const { name, login_url, username, password } = req.body;
@@ -49,9 +52,49 @@ const handleGetPartners = async (req, res) => {
     }
 };
 
-module.exports = {
+const register = async (req, res) => {
+    const { username, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await pool.query(
+        'INSERT INTO users (username, password, role) VALUES ($1, $2, $3)',
+        [username, hashedPassword, role]
+    );
+    res.status(201).send('User registered');
+};
+
+const login = async (req, res) => {
+    const { username, password } = req.body;
+    const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+    const user = result.rows[0];
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).send('Invalid credentials');
+    }
+    const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, {
+        expiresIn: '1h',
+    });
+    res.json({ token });
+};
+
+const addPartner = async (req, res) => {
+    const { name, login_url, username, password, username_selector, password_selector, login_button_selector } = req.body;
+    await pool.query(
+        'INSERT INTO partners (name, login_url, username, password, username_selector, password_selector, login_button_selector) VALUES ($1, $2, $3, $4, $5, $6, $7)',
+        [name, login_url, username, password, username_selector, password_selector, login_button_selector]
+    );
+    res.status(201).send('Partner added');
+};
+
+const getAllPartners = async (req, res) => {
+    const result = await pool.query('SELECT * FROM partners');
+    res.json(result.rows);
+};
+
+module.exports = { register, 
+    login,
+    addPartner, 
+    getAllPartners, 
     handleCreatePartner,
     handleUpdatePartner,
     handleDeletePartner,
-    handleGetPartners
+    handleGetPartners 
 };
