e632da3dd76db68ac60b6b50063dd9ebe582aa07
Natty <avi100147@gmail.com>
2024-07-25 16:49:35 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import AdminPanel from './components/AdminPanel';
+import AgentDashboard from './components/AgentDashboard';
 
-const App = () => (
+function App() {
+  return (
     <Router>
-        <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/" element={<Home />} />
-        </Routes>
+      <Switch>
+        <Route exact path="/" element={<AdminPanel/>} />
+        <Route exact path="/agent" element={<AgentDashboard/>} />
+      </Switch>
     </Router>
-);
+  );
+}
 
 export default App;
