7769ec7bb181527453907160ff4872f6f25f3e0f
Natty <avi100147@gmail.com>
2024-07-25 16:11:08 +0530

    Changed switch to routes

--- frontend/public/src/index.js
+++ frontend/public/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 import AdminPanel from './components/AdminPanel';
 import AgentDashboard from './components/AgentDashboard';
@@ -8,12 +8,12 @@ import Login from './components/Login';
 
 const Root = () => (
     <Router>
-        <Switch>
-            <Route path="/admin" component={AdminPanel} />
-            <Route path="/agent" component={AgentDashboard} />
-            <Route path="/login" component={Login} />
-            <Route path="/" component={App} />
-        </Switch>
+        <Routes>
+            <Route path="/admin" element={<AdminPanel />} />
+            <Route path="/agent" element={<AgentDashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<App />} />
+        </Routes>
     </Router>
 );
 
