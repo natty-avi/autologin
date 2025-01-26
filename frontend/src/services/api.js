e4cb7617cf582b974b05ffb31f0e512d9ed1ce24
Natty <avi100147@gmail.com>
2025-01-26 14:39:27 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://:5000/api',
+    baseURL: 'http://:5000/',
     headers: {
         'Content-Type': 'application/json'
     }
